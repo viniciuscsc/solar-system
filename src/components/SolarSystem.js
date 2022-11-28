2c78e325761f10459446782699f54a310ac2d8f7
cunhavcs <cunha.vcs@gmail.com>
2022-11-28 17:52:52 -0300

    Bônus - estiliza o SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,17 +2,20 @@ import { Component } from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import '../SolarSystem.css';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {planets
-          .map((planet, index) => {
-            const { name, image } = planet;
-            return <PlanetCard key={ index } planetName={ name } planetImage={ image } />;
-          })}
+        <div className="planets">
+          {planets
+            .map((planet, index) => {
+              const { name, image: img } = planet;
+              return <PlanetCard key={ index } planetName={ name } planetImage={ img } />;
+            })}
+        </div>
       </div>
     );
   }
