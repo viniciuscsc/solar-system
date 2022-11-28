4296118c235ff0a3e61ff545085e359eac31308f
cunhavcs <cunha.vcs@gmail.com>
2022-11-28 18:07:17 -0300

    Bônus - estiliza o Missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,11 +2,12 @@ import { Component } from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import '../Missions.css';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div className="missions" data-testid="missions">
         <Title headline="Missões" />
         { missions.map((mission, index) => {
           const { name, year, country, destination } = mission;
