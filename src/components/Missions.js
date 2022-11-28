6afeebd3c2a177687ddce68877e34d514ed86240
cunhavcs <cunha.vcs@gmail.com>
2022-11-28 16:55:20 -0300

    10 - Renderiza uma lista com as missões espaciais

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,23 @@
 import { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missions.map((mission, index) => {
+          const { name, year, country, destination } = mission;
+          return (<MissionCard
+            key={ index }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />);
+        }) }
       </div>
     );
   }
