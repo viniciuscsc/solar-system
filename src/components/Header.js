6ef4949d3d3d1864fd91b5fab12740d0065a12a8
cunhavcs <cunha.vcs@gmail.com>
2022-11-28 17:16:10 -0300

    Bônus - estiliza o Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import { Component } from 'react';
+import '../Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <header>
+      <header className="header">
         <h1>Sistema Solar</h1>
       </header>
     );
